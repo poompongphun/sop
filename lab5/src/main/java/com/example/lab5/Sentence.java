9c2a5400be3587326fa01b0b3be2c30106530d3f
poompongphun <poompongphun@gmail.com>
2023-09-05 00:48:09 +0700

    add lab5

---
+++ lab5/src/main/java/com/example/lab5/Sentence.java
@@ -0,0 +1,8 @@
+package com.example.lab5;
+
+import java.util.ArrayList;
+
+public class Sentence {
+    public ArrayList<String> badSentences = new ArrayList<String>();
+    public ArrayList<String> goodSentences = new ArrayList<String>();
+}
---
+++ lab5/src/main/java/com/example/lab5/Word.java
@@ -0,0 +1,10 @@
+package com.example.lab5;
+
+import java.util.ArrayList;
+
+public class Word {
+    public ArrayList<String> badWords = new ArrayList<String>();
+    public ArrayList<String> goodWords = new ArrayList<String>();
+
+
+}
